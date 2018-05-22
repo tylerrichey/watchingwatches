f54e47a5bac31f23bb1b8c36ebbec02d9e85dcd8
Tyler Richey <iam@tylerrichey.com>
2018-05-22 15:11:20 -0400

    refresH

---
+++ Pages/GetDataController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WatchingWatches.Pages
+{
+    [Produces("application/json")]
+    [Route("/GetData")]
+    public class GetDataController : Controller
+    {
+        public IActionResult Get()
+        {
+            CheckWatches.UpdatePrices();
+            return RedirectToAction("/Index");
+        }
+    }
+}
