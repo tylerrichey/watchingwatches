6c603355ed9af8d8edee8ccc9256a0b44721c0c8
Tyler Richey <iam@tylerrichey.com>
2019-03-22 10:25:48 -0400

    exception

---
+++ SoldOutException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WatchingWatches
+{
+    public class SoldOutException : Exception
+    {
+        public SoldOutException() : base()
+        {
+        }
+
+        public SoldOutException(string message) : base(message)
+        {
+        }
+
+        public SoldOutException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
